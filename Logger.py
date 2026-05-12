5116b22dacec58d2af7cec8901914b60cb4cfbf7
dimabel18 <dimabel.18@gmail.com>
2026-05-12 09:46:23 -0500

    Se cambio el codigo logger

--- Logger.py
+++ Logger.py
@@ -2,5 +2,8 @@
 # CLASE LOGGER
 # ============================================
 class Logger:
-   def log(self, mensaje):
-       print(f"LOG: {mensaje}")
+    def log(self, mensaje):
+        with open("logs.txt", "a", encoding="utf-8") as archivo:
+            archivo.write(f"{mensaje}\n")
+
+        print(f"LOG: {mensaje}")
